ca25e095eccf04a13ecffed0b2a7d58b15b897bd
thesuacar <suacar2005@gmail.com>
2025-10-24 13:44:09 +0200

    added preprocessing

---
+++ tests/test1.py
@@ -0,0 +1,5 @@
+from src.preprocessing_utils import clean_text
+
+# Paths (adjust if needed)
+images_path ="/Users/user/Desktop/Semester1/SE/archive/Images"
+captions_file = "/Users/user/Desktop/Semester1/SE/archive/captions.txt"
